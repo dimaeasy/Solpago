d6ff89d9f7006a08e4d0e010ed4969fb4aeeb337
dimaeasy <dimaeasy@gmail.com>
2025-07-18 19:06:56 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,25 @@
 import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message, FSInputFile
+from aiogram.filters import Command
 from config import BOT_TOKEN
 from handlers import start
 from utils.logger import logger
+from utils.qr import generate_qr_code
 
 bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
 dp = Dispatcher()
 
 # Регистрируем хендлеры
 dp.include_router(start.router)
+
+@dp.message(Command("qr"))
+async def cmd_qr(message: Message):
+    data = "https://solpago.com"  # Здесь можно вставить любой текст или ссылку
+    image_path = generate_qr_code(data)
+    
+    file = FSInputFile(image_path, filename="qrcode.png")
+    await message.answer_photo(file, caption="Вот твой QR-код!")
 
 async def main():
     logger.info("Запуск бота...")
