36b609f9d89e52988d7e537a8ae8cc2c265c6329
dimaeasy <dimaeasy@gmail.com>
2025-07-18 18:59:39 +0100

    Create qr.py

---
+++ utils/qr.py
@@ -0,0 +1,17 @@
+import qrcode
+from io import BytesIO
+
+def generate_qr(data: str) -> BytesIO:
+    qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=4
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill='black', back_color='white')
+    byte_io = BytesIO()
+    img.save(byte_io, format='PNG')
+    byte_io.seek(0)
+    return byte_io
